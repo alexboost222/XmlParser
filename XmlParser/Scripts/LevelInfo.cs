6c9f120a73eb42fd4c1fe80d953fe82633a76544
alexboost222 <alexboost222@gmail.com>
2020-03-15 19:28:09 +0300

    upd: lelve info to different file

---
+++ XmlParser/Scripts/LevelInfo.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace XmlParser.Scripts
+{
+    public struct LevelInfo
+    {
+            public List<string> TabletsText;
+            public string Name;
+            public string Pack;
+            public bool DotValid;
+            public bool JournalValid;
+            public bool SubscriptValid;
+    }
+}
