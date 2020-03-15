7c9a96ea1e2b0d6cff119188978e4f7421e1e8d3
stasiandr <stasiandr@gmail.com>
2020-03-15 18:47:18 +0300

    add: XmdInput

---
+++ XmlParser/CmdInput.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+
+namespace XmlParser
+{
+    public static class CmdInput
+    {
+        public static string GetFolderPath()
+        {
+            Console.WriteLine("Enter folder path with *.xml files in:");
+            string folderPath;
+            while (true)
+            {
+                folderPath = Console.ReadLine();
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Console.WriteLine("Directory doesn't exists, please retry:");
+                }
+                else if (Directory.GetFiles(folderPath).Length == 0)
+                {
+                    Console.WriteLine("Directory is empty, please enter folder path with *.xml files in:");
+                }
+                else
+                    break;
+            }
+            
+            return folderPath;
+        }
+        
+        public static List<XmlDocument> LoadAllXml(string directoryPath)
+        {
+            string[] files = Directory.GetFiles(directoryPath, "*.xml");
+            List<XmlDocument> answer = new List<XmlDocument>(files.Length);
+
+            foreach (string file in files)
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load(file);
+                answer.Add(xml);
+            }
+
+            return answer;
+        }
+        
+        
+    }
+}
